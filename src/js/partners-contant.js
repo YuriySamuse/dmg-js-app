2263c0e7db1495df03d558366b1560ce4af49bbc
YuriySamusew <6177385@gmail.com>
2024-04-05 18:52:37 +0200

    partners-content add import to images

--- src/js/partners-contant.js
+++ src/js/partners-contant.js
@@ -1,23 +1,33 @@
+import sinevo from '../images/partners/6.png';
+import goon from '../images/partners/3.png';
+import simplbaby from '../images/partners/2.png';
+import buhen from '../images/partners/11.jpg';
+
 const partners = [
   {
     name: 'Сінево',
-    imgURL: '../images/partners/6.png',
+    imgURL: sinevo,
     width: '270',
     URL: '#',
   },
   {
     name: 'Smile Baby',
-    imgURL: '../img/partners/Smile-Baby-NEW-2.png',
+    imgURL: simplbaby,
     width: '270',
     URL: '#',
   },
   {
     name: 'Bübchen',
-    imgURL: './images/partners/11.png',
+    imgURL: buhen,
     width: '270',
     URL: '#',
   },
-  { name: 'Goo.N', imgURL: './imgages/partners/3.png', width: '270', URL: '#' },
+  {
+    name: 'Goo.N',
+    imgURL: goon,
+    width: '270',
+    URL: '#',
+  },
   // { name: "Сінево", imgURL: "./img/partners/6.png", width: "270", URL: "#" },
   // { name: "Сінево", imgURL: "./img/partners/6.png", width: "270", URL: "#" },
   // { name: "Сінево", imgURL: "./img/partners/6.png", width: "270", URL: "#" },
