01b2b2a0845cb6c3f2f3fdfc38758409d69ec7b3
YuriySamusew <6177385@gmail.com>
2024-04-06 21:30:37 +0200

    rePush

---
+++ src/js/teams-api.js
@@ -0,0 +1,35 @@
+import volkova from '/src/images/teams/volkova-marina.png';
+import motorna from '/src/images/teams/nadiya-motorna.png';
+import mitkovska from '/src/images/teams/oksana-mitkovsaya.png';
+import kulish from '/src/images/teams/olya-kulish.png';
+import teterina from '/src/images/teams/teterina-anastasiya.png';
+
+const teams = [
+  {
+    name: 'Волкова Маріна',
+    imgURL: volkova,
+    description: 'Засновник компанії',
+  },
+  {
+    name: 'Тетерина Анастасія',
+    imgURL: teterina,
+    description: 'Регіональний менеджер, лікар – неонатолог (м. Харків)',
+  },
+  {
+    name: 'Надія Моторна',
+    imgURL: motorna,
+    description: 'Регіональний менеджер, сімейний лікар (м. Одеса)',
+  },
+  {
+    name: 'Оксана Митківська',
+    imgURL: mitkovska,
+    description: 'Керівник відділу продажу',
+  },
+  {
+    name: 'Оля Куліш',
+    imgURL: kulish,
+    description: ' ',
+  },
+];
+
+export default teams;
---
+++ src/js/teams.js
@@ -0,0 +1,51 @@
+import teams from './teams-api';
+
+const teamsContainer = document.querySelector('.js-teams');
+
+const teamCardMarkup = createTeamCardMarkup(teams);
+teamsContainer.insertAdjacentHTML('beforeend', teamCardMarkup);
+
+teamsContainer.addEventListener('mouseover', onTeamsContainerMousover);
+
+function createTeamCardMarkup(teams) {
+  return teams
+    .map(({ name, imgURL, description }) => {
+      return `
+        <li class="teams__item">
+        <img
+          src="${imgURL}"
+          alt="${name}"
+          class="teams__img"
+        />
+        <h3 class="title">${name}</h3>
+        <p>${description}</p>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+function onTeamsContainerMousover(evt) {
+  const teamsImgEl = evt.target.classList.contains('teams__img');
+  if (!teamsImgEl) {
+    return;
+  }
+
+  const teamEl = evt.target;
+  const parentTeamCard = teamEl.closest('.teams__item');
+  removeActiveCardClass();
+  addActiveCardClass(parentTeamCard);
+}
+
+function removeActiveCardClass() {
+  const currentActiveCard = document.querySelector(
+    '.teams__item.animate__animated'
+  );
+  if (currentActiveCard) {
+    currentActiveCard.classList.remove('animate__animated', 'animate__pulse');
+  }
+}
+
+function addActiveCardClass(card) {
+  card.classList.add('animate__animated', 'animate__pulse');
+}
